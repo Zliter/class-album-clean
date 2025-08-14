019023d0e13845f5ba7b6a92a5207c79a4e1d619
Zliter <110454597+Zliter@users.noreply.github.com>
2025-08-14 22:38:19 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// 这里只写一处初始化，整个站点只会用这一处
+AV.init({
+  appId: 'NQGOpixquGI0KSXYOiyasWsF-gzGzoHsz',         // 例：NQG0pixquGI0KSXY0iyasWsF-gzGZoHsz
+  appKey: 'pA03OwFMvaKyGd2HOPTdxq9V',       // 例：pA03OwFMvaKyGd2H0PTdxq9V
+  serverURL: 'https://nqgopixq.lc-cn-n1-shared.com' // 换成控制台的 REST API 服务器地址
+});
